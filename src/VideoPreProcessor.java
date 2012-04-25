1a6494f540e63c432544515ed3668f072bf6a88c
may ang <mayday815@gmail.com>
2012-04-25 00:57:54 -0700

    writes to an xml file

--- src/VideoPreProcessor.java
+++ src/VideoPreProcessor.java
@@ -4,22 +4,45 @@
 import java.awt.*;
 import java.awt.image.*;
 import java.io.*;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.xml.stream.*;
+import javax.xml.stream.events.StartDocument;
+import javax.xml.stream.events.StartElement;
+import javax.xml.stream.events.XMLEvent;
+import javax.xml.stream.events.Attribute;
+
+
 public class VideoPreProcessor {
 
-	File root;
+	File root; // root of where the videos are
+	public static int[] byteIndicies; // keeps indexes where new frames start;
 	
+	// constructor
 	public VideoPreProcessor(String folderpath) {
 		System.out.println("Video PreProcessor called");
 		root = new File(folderpath);
+		
+		// get where frames start
+    	byteIndicies = new int[720];
+    	for (int b = 0; b < 720; ++b) {
+    		byteIndicies[b] = b * 304128;
+    	}
+		
 	}
 	
 	// goes through directories, finds files and processes them
 	public void fileTraverse() {
 		System.out.println("down all the files");
-		fileRecurse(root);
+		try {
+			fileRecurse(root);
+		} catch (XMLStreamException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	
-	public void fileRecurse(File curr) {
+	private void fileRecurse(File curr) throws XMLStreamException {
 		if (curr.isDirectory()) { 
 			System.out.println(curr.getName());
 			File contents[] = curr.listFiles();
@@ -28,7 +51,7 @@ public class VideoPreProcessor {
 				processVideo(contents[0], contents[1]);
 			} else {
 				// recurse through other folders
-				for (File f : contents)  {
+				for (File f : contents)  { 
 					fileRecurse(f);
 				}
 			}
@@ -36,12 +59,55 @@ public class VideoPreProcessor {
 	}
 	
 	// process the video!
-	public void processVideo(File video, File audio) {
+	private void processVideo(File video, File audio) throws XMLStreamException {
 		if (!(video.getName().endsWith(".rgb") || audio.getName().endsWith(".wav"))) {
 			System.out.println("these files aren't valid!");
 			System.exit(1);
 		}
 		
+		String xml_name = video.getName().substring(0, video.getName().indexOf("."));
+		xml_name += ".xml";
+		// generate xml file
+		XMLOutputFactory of = XMLOutputFactory.newInstance();
+		XMLEventWriter ew = null;
+		try {
+			ew = of.createXMLEventWriter(new FileOutputStream(xml_name));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (XMLStreamException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		XMLEventFactory ef = XMLEventFactory.newInstance();
+		XMLEvent end = ef.createDTD("\n");
+		// start tag
+		StartDocument startDoc = ef.createStartDocument();
+		ew.add(startDoc);
+		ew.add(end);
+		// <video>
+		StartElement vidElem = ef.createStartElement("", "", "Video");
+		ew.add(vidElem);
+		ew.add(end);
+		// for each frame <frame no=i> </frame>
+		for (int i = 0; i < 720; ++i) {
+			Attribute frameNo = ef.createAttribute("no", Integer.toString(i));
+			List<Attribute> attrList = Arrays.asList(frameNo);
+			List<Object> nsList = Arrays.asList();
+			StartElement frameElem = ef.createStartElement("", "", "Frame",  attrList.iterator(), nsList.iterator());
+			ew.add(frameElem);
+			ew.add(end);
+			// TODO: video descriptors
+			ew.add(ef.createEndElement("", "", "Frame"));
+			ew.add(end);
+		}
+		
+		// </video>
+		ew.add(ef.createEndElement("", "", "Video"));
+		ew.add(end);
+		// end document
+		ew.add(ef.createEndDocument());
+		ew.close();
 	}
 	
 }
